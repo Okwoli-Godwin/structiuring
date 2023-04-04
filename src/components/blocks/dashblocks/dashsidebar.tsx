3b785fc74491a4fb22a81ed9eb0b1c92dbb10ec0
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-05 00:08:42 +0100

    pushed

--- src/components/blocks/dashblocks/dashsidebar.tsx
+++ src/components/blocks/dashblocks/dashsidebar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const dashsidebar = () => {
-  return (
-    <div>dashsidebar</div>
-  )
+  return 
+      <Container>
+          🤗☺🤗🤗🤗☺
+    </Container>
+  
 }
 
 export default dashsidebar
+
+const Container = styled.div`
+    width: 250px;
+    height: 100vh;
+    background-color: blue;
+    position: fixed;
+`
