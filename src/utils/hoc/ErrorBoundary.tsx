7cbaa2080d8cfa5453585b25aa6d28b5b36f0250
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-04 23:11:43 +0100

    pushed

---
+++ src/utils/hoc/ErrorBoundary.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorBoundary = () => {
+  return (
+      <div>
+          <p>OPPS...</p>
+          <h2>Something went wrong....</h2>
+    </div>
+  )
+}
+
+export default ErrorBoundary
