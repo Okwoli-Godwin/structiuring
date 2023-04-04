9e3bfd329299ea3bab9793bbe6bdab71d0d67d47
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-05 00:11:20 +0100

    updated

--- src/components/blocks/dashblocks/dashhead.tsx
+++ src/components/blocks/dashblocks/dashhead.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const dashhead = () => {
   return (
-    <div>dashhead</div>
+      <Container>
+          🍗🍖🍗🍖🥵🥵
+    </Container>
   )
 }
 
 export default dashhead
+
+const Container = styled.div`
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: green;
+`
+
