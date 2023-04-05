29e75e024877e4500b364d5a14b2bcebea504e85
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-05 07:41:46 +0100

    updated

--- src/components/blocks/dashblocks/Dashbody.tsx
+++ src/components/blocks/dashblocks/Dashbody.tsx
@@ -3,8 +3,19 @@ import styled from 'styled-components'
 
 const Dashbody = () => {
   return (
-    <div>D</div>
+      <Container>
+          🤗😁🤣👏👍
+    </Container>
   )
 }
 
 export default Dashbody
+
+const Container = styled.div`
+    width: calc(100% - 300px);
+    display: flex;
+    height: 100vh;
+    background-color: gray;
+    justify-content: center;
+    align-items: center;
+`
