235493f6dc21f8e7e71a7b34bfb902e716249bc5
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-05 07:56:18 +0100

    updated

---
+++ src/components/layout/DashLayout/Dashlayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Dashbody, Dashsidebar } from '../../blocks'
+
+const Dashlayout = () => {
+  return (
+      <Container>
+          
+    </Container>
+  )
+}
+
+export default Dashlayout
+const Container = styled.div`
+    
+`
