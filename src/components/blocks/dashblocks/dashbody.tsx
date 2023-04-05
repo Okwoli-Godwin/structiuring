58e4b96042128e4c6948448b20ee505dd53dd69f
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-05 07:14:21 +0100

    updated

--- src/components/blocks/dashblocks/dashbody.tsx
+++ src/components/blocks/dashblocks/dashbody.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from "styled-components"
 
 const dashbody = () => {
   return (
-    <div>dashbody</div>
+      <Container>
+          😎😎😒🤷‍♀️
+    </Container>
   )
 }
 
 export default dashbody
+
+const Container = styled.div`
+    width: calc(100% - 250px);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+`
